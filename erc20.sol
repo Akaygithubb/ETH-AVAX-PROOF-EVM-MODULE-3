510cca3d19860d02ddc4bc37f9da82b97a4b5a84
AIKAKSHWER VIVEK <22BCS17136@cuchd.in>
2024-06-19 15:57:53 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DevToken is ERC20, Ownable {
+contract MyToken is ERC20, Ownable {
     constructor(address initialOwner) ERC20("MyToken", "MTK") Ownable(initialOwner) {
         _mint(msg.sender, 1000 * 10**18); // Mint initial supply to the deployer
     }
