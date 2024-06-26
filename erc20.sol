23bec204ade4236eb042692954626d4556bdad34
AIKAKSHWER VIVEK <22BCS17136@cuchd.in>
2024-06-26 07:01:13 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -5,8 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor(address initialOwner) ERC20("MyToken", "MTK") Ownable(initialOwner) {
+    constructor(address initialOwner) ERC20("MyToken", "MTK") Ownable() {
+        _transferOwnership(initialOwner);
         _mint(msg.sender, 1000 * 10**18); // Mint initial supply to the deployer
+    }
+
+    // Explicitly defined transfer function
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
     }
 
     // Function to mint tokens (only owner)
@@ -24,4 +31,3 @@ contract MyToken is ERC20, Ownable {
         _burn(account, amount);
     }
 }
-
